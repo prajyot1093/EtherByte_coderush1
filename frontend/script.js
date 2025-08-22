8813fa1efd6b5e628d38ef43347535ae455df4c3
PRAJYOT PUNDE <prajyot1093@gmail.com>
2025-08-22 13:03:04 +0530

    doc  get element

---
+++ frontend/script.js
@@ -0,0 +1,5 @@
+document.getElementById("uploadForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const fileInput = document.getElementById("fileInput");
+    const outputDiv = document.getElementById("output");
